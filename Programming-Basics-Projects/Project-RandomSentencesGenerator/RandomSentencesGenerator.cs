88971ef096291e2b838ca8d87f81c4410fdbd335
PetarPaunov <petarp.2000@abv.bg>
2022-05-14 18:12:42 +0300

    Add project RandomSentancesGenerator

---
+++ Programming-Basics-Projects/Project-RandomSentencesGenerator/RandomSentencesGenerator.cs
@@ -0,0 +1,32 @@
+﻿string[] names = { "Peter", "Michell", "Jane", "Steve", };
+string[] places = { "Sofiq", "Plovdiv", "Varna", "Burags" };
+string[] verbs = { "eats", "holds", "sees", "plays with", "brings" };
+string[] nouns = { "stones", "cake", "apple", "laptop", "bikes" };
+string[] adverbs = { "slowly", "diligently", "warmly", "sadly", "rapidly" };
+string[] details = { "near the river", "at home", "in the park" };
+
+while (true)
+{
+    string name = GetRndomWord(names);
+    string place = GetRndomWord(places);
+    string verb = GetRndomWord(verbs);
+    string noun = GetRndomWord(nouns);
+    string adverb = GetRndomWord(adverbs);
+    string detail = GetRndomWord(details);
+
+    string who = $"{name} from {place}";
+    string action = $"{adverb} {verb} {noun}";
+    string sentence = $"{who} {action} {detail}.";
+
+    Console.WriteLine(sentence);
+
+    Console.ReadLine();
+}
+
+
+string GetRndomWord(string[] words)
+{
+    Random random = new Random();
+    string word = words[random.Next(words.Length)];
+    return word;
+}
