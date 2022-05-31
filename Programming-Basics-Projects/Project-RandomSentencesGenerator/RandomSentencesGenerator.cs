afd50a375a04c5906ab6b16aff1e2a2a10bd8755
PetarPaunov <petarp.2000@abv.bg>
2022-05-31 17:10:55 +0300

    Small fixes

--- Programming-Basics-Projects/Project-RandomSentencesGenerator/RandomSentencesGenerator.cs
+++ Programming-Basics-Projects/Project-RandomSentencesGenerator/RandomSentencesGenerator.cs
@@ -1,5 +1,5 @@
 ﻿string[] names = { "Peter", "Michell", "Jane", "Steve" };
-string[] places = { "Sofiq", "Plovdiv", "Varna", "Burags" };
+string[] places = { "Sofia", "Plovdiv", "Varna", "Burgas" };
 string[] verbs = { "eats", "holds", "sees", "plays with", "brings" };
 string[] nouns = { "stones", "cake", "apple", "laptop", "bikes" };
 string[] adverbs = { "slowly", "diligently", "warmly", "sadly", "rapidly" };
@@ -9,16 +9,16 @@ Console.WriteLine("Hello, this is your first random-generated sentence: ");
 
 while (true)
 {
-    string name = GetRandomWord(names);
-    string place = GetRandomWord(places);
-    string verb = GetRandomWord(verbs);
-    string noun = GetRandomWord(nouns);
-    string adverb = GetRandomWord(adverbs);
-    string detail = GetRandomWord(details);
+    string randomName = GetRandomWord(names);
+    string randomPlace = GetRandomWord(places);
+    string randomVerb = GetRandomWord(verbs);
+    string randomNoun = GetRandomWord(nouns);
+    string randomAdverb = GetRandomWord(adverbs);
+    string randomDetail = GetRandomWord(details);
 
-    string who = $"{name} from {place}";
-    string action = $"{adverb} {verb} {noun}";
-    string sentence = $"{who} {action} {detail}.";
+    string who = $"{randomName} from {randomPlace}";
+    string action = $"{randomAdverb} {randomVerb} {randomNoun}";
+    string sentence = $"{who} {action} {randomDetail}.";
 
     Console.WriteLine(sentence);
     Console.WriteLine("Click [Enter] to generate a new one.");
@@ -26,9 +26,10 @@ while (true)
     Console.ReadLine();
 }
 
-static string GetRandomWord(string[] words)
+string GetRandomWord(string[] words)
 {
     Random random = new Random();
-    string word = words[random.Next(words.Length)];
+    int randomIndex = random.Next(words.Length);
+    string word = words[randomIndex];
     return word;
 }
