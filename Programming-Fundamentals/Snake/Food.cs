33bbe7b22e149c07d4cdc774acd8de09e5688c5b
PetarPaunov <petarp.2000@abv.bg>
2022-10-24 17:33:12 +0300

    Snake game added

---
+++ Programming-Fundamentals/Snake/Food.cs
@@ -0,0 +1,23 @@
+﻿namespace Snake
+{
+    public class Food
+    {
+        private Random random;
+        private char[] foodSymbols;
+
+        public Food(int windowHeight, int windowWidth)
+        {
+            this.foodSymbols = new char[] { '#', '@', '%', '*', '&' };
+            this.random = new Random();
+
+            this.XFoodCoordinate = random.Next(1, windowHeight);
+            this.YFoodCoordinate = random.Next(1, windowWidth);
+
+            this.FoodSymbol = this.foodSymbols[random.Next(0, this.foodSymbols.Length)];
+        }
+
+        public int XFoodCoordinate { get; set; }
+        public int YFoodCoordinate { get; set; }
+        public char FoodSymbol { get; set; }
+    }
+}
